e4c6cf66f9e7d8d33de3e98130a6bd6f176249ac
Artemiev Dima <adbart1985@gmail.com>
2023-10-19 21:21:49 +0300

    add seminar1/hw1

---
+++ src/test/java/task1/task1Test.java
@@ -0,0 +1,11 @@
+package task1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+public class task1Test {
+    @Test
+    public void testMethod() {
+        assertEquals(5, 2 + 3);
+    }
+}
